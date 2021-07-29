9d1268f91fe004e398735e0130a1b1e4435a2845
egonzalez <egonzalez@msi.notebook.hiperion.local>
2021-07-29 14:31:50 -0300

    Correct headings comments

--- hpsdr/hpsdr_oldprotocol.c
+++ hpsdr/hpsdr_oldprotocol.c
@@ -1,8 +1,28 @@
 /*
- * hpsdr_oldprotocol.c
+ * Copyright 2021 Emiliano Gonzalez LU3VEA (lu3vea @ gmail . com))
+ * * Project Site: https://github.com/hiperiondev/hpsdr-rpitx *
  *
- *  Created on: 29 jul. 2021
- *      Author: Emiliano Gonzalez LU3VEA (lu3vea @ gmail . com))
+ * This is based on other projects:
+ *    librpitx (https://github.com/F5OEO/librpitx)
+ *    HPSDR simulator (https://github.com/g0orx/pihpsdr)
+ *
+ *    please contact their authors for more information.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ *
  */
 
 #include <stdint.h>
